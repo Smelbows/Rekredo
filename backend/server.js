1a9a3e9b809bd2d9a22d7b909bba067fa1fae90a
jakobxlindstrom <jake@lindstroem.se>
2022-01-18 10:36:10 +0100

    adding more endpoints

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,23 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import cors from "cors";
-import crypto from "crypto";
-import bcrypt from "bcrypt";
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import crypto from 'crypto';
+import bcrypt from 'bcrypt';
 
-const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/rekredo'
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.Promise = Promise
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/rekredo';
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
 
 // Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 //schemas
 const UserSchema = new mongoose.Schema({
@@ -30,88 +30,77 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
- accountType: {
-   type: String, 
-   enum: [
-     "Business",
-     "Personal",
-   ],
-   required: true,
- },
+  accountType: {
+    type: String,
+    enum: ['Business', 'Personal'],
+    required: true,
+  },
   accessToken: {
     type: String,
     default: () => crypto.randomBytes(128).toString('hex'),
   },
-})
+});
 
 const ProductSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
-  }, 
+  },
   description: {
-    type: String, 
+    type: String,
     required: true,
   },
   category: {
-    type: String, 
+    type: String,
     required: true,
-    enum: [
-      "Instrument",
-      "Vehicle",
-      "Clothing",
-      "Electronics",
-      "Art",
-      "Toys",
-    ],
+    enum: ['Instrument', 'Vehicle', 'Clothing', 'Electronics', 'Art', 'Toys'],
   },
   tags: {
     type: Array,
   },
-})
-
-const User = mongoose.model('User', UserSchema)
-const Product = mongoose.model('Product', ProductSchema)
-
-
-const authenticateUser = async (req, res, next) => {
-  const accessToken = req.header('Authorization')
-  try {
-    const user = await User.findOne({ accessToken })
-    if (user) {
-      next()
-    } else {
-      res.status(401).json({ response: 'Please log in', success: false })
-    }
-  } catch (error) {
-    res.status(400).json({ response: error, success: false })
-  }
-}
-
-app.get("/", (req, res) => {
-  res.send("Homepage");
 });
 
-app.get('/account', authenticateUser)
-app.get("/account", (req, res) => {
-  res.send("this is your account page");
+const User = mongoose.model('User', UserSchema);
+const Product = mongoose.model('Product', ProductSchema);
+
+const authenticateUser = async (req, res, next) => {
+  const accessToken = req.header('Authorization');
+  try {
+    const user = await User.findOne({ accessToken });
+    if (user) {
+      next();
+    } else {
+      res.status(401).json({ response: 'Please log in', success: false });
+    }
+  } catch (error) {
+    res.status(400).json({ response: error, success: false });
+  }
+};
+
+app.get('/', (req, res) => {
+  res.send('Homepage');
+});
+
+app.get('/account', authenticateUser);
+app.get('/account', (req, res) => {
+  res.send('this is your account page');
 });
 
 app.post('/register', async (req, res) => {
-  const { username, password, accountType } = req.body
+  const { username, password, accountType } = req.body;
 
   try {
-    const salt = bcrypt.genSaltSync()
+    const salt = bcrypt.genSaltSync();
 
     if (password.length < 5) {
-      throw 'Password must be at least 5 characters long'
+      throw 'Password must be at least 5 characters long';
     }
 
     const newUser = await new User({
       username,
       password: bcrypt.hashSync(password, salt),
       accountType,
-    }).save()
+    }).save();
 
     res.status(201).json({
       response: {
@@ -120,16 +109,16 @@ app.post('/register', async (req, res) => {
         accessToken: newUser.accessToken,
       },
       success: true,
-    })
+    });
   } catch (error) {
-    res.status(400).json({ response: error, success: false })
+    res.status(400).json({ response: error, success: false });
   }
-})
+});
 
 app.post('/log-in', async (req, res) => {
-  const { username, password } = req.body
+  const { username, password } = req.body;
   try {
-    const user = await User.findOne({ username })
+    const user = await User.findOne({ username });
     if (user && bcrypt.compareSync(password, user.password)) {
       res.status(200).json({
         response: {
@@ -138,22 +127,57 @@ app.post('/log-in', async (req, res) => {
           accessToken: user.accessToken,
         },
         success: true,
-      })
+      });
     } else {
       res.status(404).json({
         response: 'Username or password does not match',
         success: false,
-      })
+      });
     }
   } catch (error) {
-    res.status(404).json({ response: error, success: false })
+    res.status(404).json({ response: error, success: false });
   }
-})
+});
 
+app.post('/upload', async (req, res) => {
+  const { name, description, category, tags } = req.body;
+  try {
+    if (!name) {
+      throw 'Your product has to have a name';
+    }
+    if (!description) {
+      throw 'Your product has to have a description';
+    }
+    if (!category) {
+      throw 'please choose a category for your product';
+    }
+    if (Array.isArray(tags) === false) {
+      throw 'please add tags as an array';
+    }
 
+    const newProduct = await new Product({
+      name,
+      description,
+      category,
+      tags,
+    }).save();
+    res.status(201).json({
+      response: {
+        productId: newProduct._id,
+        name: newProduct.name,
+        description: newProduct.description,
+        category: newProduct.category,
+        tags: newProduct.tags,
+      },
+      success: true,
+    });
+  } catch (error) {
+    res.status(404).json({ response: error, success: false });
+  }
+});
 
 // Start the server
 app.listen(port, () => {
   // eslint-disable-next-line
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
