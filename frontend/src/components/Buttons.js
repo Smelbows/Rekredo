a95c6bf7b3f59a13fc8f8f47af0f7463638d5734
jakobxlindstrom <jake@lindstroem.se>
2022-01-19 16:37:08 +0100

    try

---
+++ frontend/src/components/Buttons.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  display: inline-block;
+  border-radius: 3px;
+  padding: 0.5rem 0;
+  margin: 0.5rem 1rem;
+  width: 11rem;
+  background: transparent;
+  color: white;
+  border: 2px solid white;
+`;
+
+export const SmallButton = styled.a`
+  display: inline-block;
+  border-radius: 3px;
+  padding: 0.5rem 0;
+  margin: 0.5rem 1rem;
+  width: 11rem;
+  background: transparent;
+  color: white;
+  border: 2px solid white;
+`;
+
+export const PropButton = styled.button`
+  display: inline-block;
+  border-radius: 3px;
+  padding: 0.5rem 0;
+  margin: 0.5rem 1rem;
+  width: 11rem;
+  background: transparent;
+  color: ${(props) => props.propBtnColor || 'red'};
+  border: 2px solid white;
+`;
--- frontend/src/pages/Account.js
+++ frontend/src/pages/Account.js
@@ -1,7 +1,31 @@
-// import react from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import { Link, useNavigate } from 'react-router-dom';
+
+import { Button, PropButton } from '../components/Buttons';
+
+const ProfileCard = styled.div`
+  display: flex;
+  border-radius: 3px;
+  padding: 0.5rem 0;
+  margin: 0.5rem 1rem;
+  width: 20rem;
+  background: grey;
+  color: white;
+  border: 2px solid green;
+`;
 
 const Account = () => {
-  return <div>Account</div>;
+  const navigate = useNavigate();
+  return (
+    <ProfileCard>
+      <Button onClick={() => navigate('/home')}>
+        <Link to="/home">Visit your home</Link>Hello
+      </Button>
+      <PropButton propBtnColor="yellow"> rekredo</PropButton>
+      Account
+    </ProfileCard>
+  );
 };
 
 export default Account;
