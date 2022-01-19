e464c77b04e8dac7c5781cdc36c621ba921d29b4
jakobxlindstrom <jake@lindstroem.se>
2022-01-19 13:15:32 +0100

    added endpoints

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,12 @@ import cors from 'cors';
 import crypto from 'crypto';
 import bcrypt from 'bcrypt';
 import multer from 'multer';
+// import fs from 'fs';
+// import path from 'path';
+import dotenv from 'dotenv';
+
+import cloudinaryFramework from 'cloudinary';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
 // import bodyparser from 'bodyparser';
 
 const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/rekredo';
@@ -16,11 +22,30 @@ mongoose.Promise = Promise;
 //   PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
+dotenv.config();
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 // app.use(bodyparser.json());
+
+// Setting up our cloudinary storage
+const cloudinary = cloudinaryFramework.v2;
+cloudinary.config({
+  cloud_name: 'rekredo',
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: 'props',
+    allowedFormats: ['jpg', 'png', 'JPG', 'jpeg'],
+    transformation: [{ width: 500, height: 500, crop: 'limit' }],
+  },
+});
+const parser = multer({ storage });
 
 //schemas
 const UserSchema = new mongoose.Schema({
@@ -64,39 +89,39 @@ const ProductSchema = new mongoose.Schema({
   owner: {
     type: String,
   },
-  status: AvailabilitySchema,
+  // status: AvailabilitySchema,
 });
 
-const AvailabilitySchema = new mongoose.Schema({
-  status: {
-    type: String,
-  },
-  availability: { type: Boolean },
-});
+// const AvailabilitySchema = new mongoose.Schema({
+//   status: {
+//     type: String,
+//   },
+//   availability: { type: Boolean },
+// });
 
-const ImageSchema = new mongoose.Schema({
-  name: String,
-  desc: String,
-  img: {
-    data: Buffer,
-    contentType: String,
-  },
-});
+// const ImageSchema = new mongoose.Schema({
+//   name: String,
+//   desc: String,
+//   img: {
+//     data: Buffer,
+//     contentType: String,
+//   },
+// });
 
 const User = mongoose.model('User', UserSchema);
 const Product = mongoose.model('Product', ProductSchema);
-const Image = mongoose.model('Image', ImageSchema);
+// const Image = mongoose.model('Image', ImageSchema);
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads');
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.fieldname + '-' + Date.now());
-  },
-});
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, 'uploads');
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, file.fieldname + '-' + Date.now());
+//   },
+// });
 
-const upload = multer({ storage: storage });
+// const upload = multer({ storage: storage });
 
 const authenticateUser = async (req, res, next) => {
   const accessToken = req.header('Authorization');
@@ -121,16 +146,16 @@ app.get('/account', (req, res) => {
   res.send('this is your account page');
 });
 
-app.get('/', (req, res) => {
-  Image.find({}, (err, items) => {
-    if (err) {
-      console.log(err);
-      res.status(500).send('An error occurred', err);
-    } else {
-      res.render('imagesPage', { items: items });
-    }
-  });
-});
+// app.get('/imgget', (req, res) => {
+//   Image.find({}, (err, items) => {
+//     if (err) {
+//       console.log(err);
+//       res.status(500).send('An error occurred', err);
+//     } else {
+//       res.render('imagesPage', { items: items });
+//     }
+//   });
+// });
 
 app.post('/register', async (req, res) => {
   const { username, password, accountType } = req.body;
@@ -221,6 +246,46 @@ app.post('/upload', async (req, res) => {
     res.status(404).json({ response: error, success: false });
   }
 });
+
+const Prop = mongoose.model('Prop', {
+  name: String,
+  imageUrl: String,
+  imageId: String,
+});
+
+app.post('/props', parser.single('image'), async (req, res) => {
+  try {
+    const prop = await new Prop({
+      name: req.body.name,
+      imageUrl: req.file.path,
+      imageId: req.file.filename,
+    }).save();
+    res.json(prop);
+  } catch (err) {
+    res.status(400).json({ errors: err.errors });
+  }
+});
+
+// app.post('/uploadimg', upload.single('image'), (req, res, next) => {
+//   const image = {
+//     name: req.body.name,
+//     desc: req.body.desc,
+//     img: {
+//       data: fs.readFileSync(
+//         path.join(__dirname + '/uploads/' + req.file.filename)
+//       ),
+//       contentType: 'image/png',
+//     },
+//   };
+//   Image.create(image, (err, item) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       // item.save();
+//       res.redirect('/');
+//     }
+//   });
+// });
 
 // Start the server
 app.listen(port, () => {
