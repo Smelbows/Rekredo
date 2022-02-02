00ca23ff4bebee4c3e79f22b6b9bf94635180e73
loulunds <lousanne.lundstrom@gmail.com>
2022-02-02 11:51:49 +0100

    changing personal and business schemas to one user schema

--- backend/models/models.js
+++ backend/models/models.js
@@ -1,73 +1,66 @@
 import mongoose from 'mongoose';
 import crypto from 'crypto';
-import { type } from 'os';
+// import { type } from 'os';
 
-const PersonalSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
   username: {
     type: String,
     unique: true,
-    required: true,
+    required: true
   },
   password: {
     type: String,
-    required: true,
+    required: true
   },
   email: {
     type: String,
-    required: true,
+    required: true
   },
   accessToken: {
     type: String,
-    default: () => crypto.randomBytes(128).toString('hex'),
+    default: () => crypto.randomBytes(128).toString('hex')
   },
-  ownedProducts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }],
+  accountType: {
+    type: String,
+    required: true,
+    enum: ['Business', 'Personal']
+  },
+  business: BusinessSchema,
+  personal: PersonalSchema
+});
+
+const PersonalSchema = new mongoose.Schema({
+  ownedProducts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }]
 });
 
 const BusinessSchema = new mongoose.Schema({
-  businessName: {
-    type: String,
-    unique: true,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  businessEmail: {
-    type: String,
-    required: true,
-  },
   location: {
-    type: String,
+    type: String
   },
   vatNumber: {
-    type: Number,
+    type: Number
   },
-  accessToken: {
-    type: String,
-    default: () => crypto.randomBytes(128).toString('hex'),
-  },
-  myOrders: [{type: mongoose.Schema.Types.ObjectId, ref: 'Order'}]
+  myOrders: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Order' }]
 });
 
 const ImageSchema = new mongoose.Schema({
   imageUrl: {
     type: String,
-    required: true,
+    required: true
   },
   imageId: {
-    type: String,
-  },
+    type: String
+  }
 });
 
 const ProductSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    required: true
   },
   description: {
     type: String,
-    required: true,
+    required: true
   },
   category: {
     type: String,
@@ -79,41 +72,41 @@ const ProductSchema = new mongoose.Schema({
       'Electronics',
       'Art',
       'Toys',
-      'Environment',
-    ],
+      'Environment'
+    ]
   },
   tags: {
-    type: Array,
+    type: Array
   },
   owner: {
-    type: String,
+    type: String
   },
   image: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'Image',
+    ref: 'Image'
   },
   availability: {
     type: Boolean,
-    default: true,
+    default: true
   },
   createdAt: {
     type: Number,
-    default: () => Date.now(),
-  },
+    default: () => Date.now()
+  }
 });
 
 const OrderSchema = new mongoose.Schema({
   products: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }],
-  orderNumber: {type: Number},
+  orderNumber: { type: Number },
   createdAt: { type: Number, default: () => Date.now() },
-  completed: {type: Boolean, default: false}
+  completed: { type: Boolean, default: false }
 });
 
-
-const PersonalUser = mongoose.model('PersonalUser', PersonalSchema);
-const BusinessUser = mongoose.model('BusinessUser', BusinessSchema);
+// const PersonalUser = mongoose.model('PersonalUser', PersonalSchema);
+// const BusinessUser = mongoose.model('BusinessUser', BusinessSchema);
+const User = mongoose.model('User', UserSchema);
 const Product = mongoose.model('Product', ProductSchema);
 const Image = mongoose.model('Image', ImageSchema);
-const Order = mongoose.model('Order', OrderSchema)
+const Order = mongoose.model('Order', OrderSchema);
 
-module.exports = { PersonalUser, BusinessUser, Product, Image, Order };
+module.exports = { User, Product, Image, Order };
