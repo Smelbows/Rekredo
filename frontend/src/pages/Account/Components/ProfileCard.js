5c54adb7b6cd19bec5d3460b15da1b905fb06db4
Sarah Mottram <smelbows@gmail.com>
2022-02-04 14:04:28 +0100

    added start of profile card

--- frontend/src/pages/Account/Components/ProfileCard.js
+++ frontend/src/pages/Account/Components/ProfileCard.js
@@ -1,24 +1,27 @@
 import React from 'react';
 // import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-
+import { useSelector } from 'react-redux';
 import { StyledProfileCard } from 'styledElements/Card';
 import { Button, PropButton } from '../../../styledElements/Buttons';
+import { H2 } from 'styledElements/Texts';
 
 const ProfileCard = () => {
-    const navigate = useNavigate();
-    //   const user = useSelector((state) => state.user);
-    return (
-        <StyledProfileCard>
-            <div>
-                <Button onClick={() => navigate('/contact')}>
-                    need help? click here
-                </Button>
-                <PropButton propBtnColor='yellow'> This is a propbutton</PropButton>
-                Account
-            </div>
-        </StyledProfileCard>
-    );
+  const navigate = useNavigate();
+  const user = useSelector((state) => state.user);
+  console.log(user);
+  return (
+    <StyledProfileCard>
+      <div>
+          <H2>Account Details</H2>
+        <Button onClick={() => navigate('/contact')}>
+          need help? click here
+        </Button>
+        <PropButton propBtnColor="yellow"> This is a propbutton</PropButton>
+        Account
+      </div>
+    </StyledProfileCard>
+  );
 };
 
 export default ProfileCard;
