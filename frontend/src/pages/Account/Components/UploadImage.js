ee8d9ef43c31ba72cbcfd611696d8a5a8c99e234
Lousanne Lundström <56274821+loulunds@users.noreply.github.com>
2022-03-06 12:48:55 +0100

    Background button change

--- frontend/src/pages/Account/Components/UploadImage.js
+++ frontend/src/pages/Account/Components/UploadImage.js
@@ -51,7 +51,7 @@ export const UploadImage = () => {
       <Form onSubmit={handleFormSubmit}>
         <Label>Prop Image</Label>
         <Input type="file" ref={fileInput} required />
-        <Button color="black" type="submit">Upload Image</Button>
+        <Button background="grey" type="submit">Upload Image</Button>
       </Form>
       {image && (
         <img
